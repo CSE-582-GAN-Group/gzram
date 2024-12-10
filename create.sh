0da1dc0b71481ba2879a16a9c1ed4927d51d9cf8
Nolan <nkuza8@gmail.com>
2024-12-09 22:26:59 -0500

    Update scripts

--- create.sh
+++ create.sh
@@ -5,4 +5,4 @@ set -e
 (cd build && make)
 echo 1 | sudo tee -a /sys/class/zspool/zspool0/reset
 echo 2G | sudo tee -a /sys/class/zspool/zspool0/disksize
-sudo ./build/server/gzram_server 2147483648
+sudo ./build/server/gzram_server 2147483648 /dev/zspool0
--- setup_drivers.sh
+++ setup_drivers.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
+
+echo "# Remove ulbk"
 sudo modprobe -r ublk_drv
+
+echo "# Remove zspool"
 sudo rmmod zspool_drv.ko
+
+echo "# Build zspool"
 (cd zspool && make)
-sudo modprobe ublk_drv
+
+echo "# Insert zspool"
 sudo insmod zspool/zspool_drv.ko
+
+echo "# Insert ublk"
+sudo modprobe ublk_drv
